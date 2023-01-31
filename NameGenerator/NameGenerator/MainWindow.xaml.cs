519de7351828e08feb28b415b617e91b847e6f1b
szgery <szabogergely805@gmail.com>
2023-01-31 20:06:14 +0100

    Generating names

--- NameGenerator/NameGenerator/MainWindow.xaml.cs
+++ NameGenerator/NameGenerator/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +26,49 @@ namespace NameGenerator
         {
             InitializeComponent();
         }
+
+        List<string> lastNames = new List<string>(); 
+        List<string> firstNames = new List<string>();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                foreach (var nev in File.ReadAllLines(ofd.FileName))
+                {
+                    firstNames.Add(nev);
+                }
+                lbFirstnames.ItemsSource = firstNames;
+            }
+        }
+
+        private void btnLoadLast_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                foreach (var nev in File.ReadAllLines(ofd.FileName))
+                {
+                    lastNames.Add(nev);
+                }
+                lbLastnames.ItemsSource = lastNames;
+            }
+        }
+
+        int FirstnameIndex;
+        int LastnameIndex;
+
+        private void btnGenerateNames_Click(object sender, RoutedEventArgs e)
+        {
+            Random r = new Random();
+
+            FirstnameIndex = r.Next(lbFirstnames.Items.Count);
+            LastnameIndex = r.Next(lbLastnames.Items.Count);
+
+            lbFullnames.Items.Add(lbFirstnames.Items[FirstnameIndex].ToString() + " " + lbLastnames.Items[LastnameIndex].ToString());
+            lbFirstnames.SelectedItem = lbFirstnames.Items[FirstnameIndex];
+            lbLastnames.SelectedItem = lbLastnames.Items[LastnameIndex];
+        }                
     }
 }
